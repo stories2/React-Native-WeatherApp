1ba9bd9e547a5f4b51eefcad52f1480bf5da71f7
Kim HyunWoo <stories2@naver.com>
2019-04-30 16:18:23 +0900

    날씨 데이터를 외부 API 로 부터 받아옴

--- fucking-weather/App.js
+++ fucking-weather/App.js
@@ -3,19 +3,21 @@ import { StyleSheet, Text, View, ActivityIndicator, StatusBar } from 'react-nati
 
 import Weather from './Weather';
 
+const WEATHER_API_KEY = "70c844fdcfeb46b7f41aa7b47278e97e"
+
 export default class App extends Component {
   state = {
     isLoaded: false,
     error: null,
+    temperature: null,
+    name: null
   }
 
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       position => {
-        this.setState({        
-          isLoaded: true
-        })
         console.log("geo", position)
+        this._getWeather(position.coords.latitude, position.coords.longitude)
       },
       error => {
         console.log("geo", error)
@@ -23,6 +25,34 @@ export default class App extends Component {
           error: error
         })
       })
+  }
+
+  _getWeather = (lat, long) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${WEATHER_API_KEY}`)
+    .then(response => {
+      return response.json()
+    })
+    .then(json => {
+      console.log("weather", json)
+      if(json.cod !== 200) {
+        this.setState({
+          error: json
+        })
+      }
+      else {
+        this.setState({
+          temperature: json.main.temp,
+          name: json.weather[0].main,
+          isLoaded: true
+        })
+        console.log("state", this.state)
+      }
+    })
+    .catch(error => {
+      this.setState({
+        error: error
+      })
+    })
   }
 
   render() {

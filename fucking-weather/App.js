469066e8edcfe39131d300db2cfc68f589d6cb4f
Kim HyunWoo <stories2@naver.com>
2019-04-30 15:00:33 +0900

    로딩 레이아웃 구현

--- fucking-weather/App.js
+++ fucking-weather/App.js
@@ -1,44 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  state = {
+    isLoaded: false
+  }
   render() {
+    const { isLoaded } = this.state;
+
     return (
       <View style={styles.container}>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
-        <View style={styles.redView}/>
-        <View style={styles.yellowView}/>
+        { isLoaded ? null : 
+        <View style={styles.loading}>
+          <Text style={styles.loadingText}>Getting the fucking weather.</Text>
+        </View>
+      }
       </View>
     );
   }
@@ -48,23 +24,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    flexDirection: 'row',
-    justifyContent: 'flex-start', //flex-start, flex-end, space-between, space-around, center
-    alignItems: 'center',
-    flexWrap: 'nowrap'
   },
-  redView: {
-    // flex: 1,
-    width: 50,
-    height: 50,
-    backgroundColor: 'red',
-    // alignSelf: 'flex-start'
+  loading: {
+    flex: 1,
+    backgroundColor: '#FDF6AA',
+    justifyContent: "flex-end",
+    // paddingRight: 25,
+    paddingLeft: 25
   },
-  yellowView: {
-    // flex: 1,
-    width: 50,
-    height: 50,
-    backgroundColor: 'yellow',
-    // alignSelf: 'flex-end'
+  loadingText: {
+    fontSize: 38,
+    marginBottom: 100
   }
 });

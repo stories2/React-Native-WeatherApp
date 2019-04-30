c8f0ac70b35ea1514c2627f250aade20f3026875
Kim HyunWoo <stories2@naver.com>
2019-04-30 14:45:08 +0900

    레이아웃 이것저것

--- fucking-weather/App.js
+++ fucking-weather/App.js
@@ -7,6 +7,38 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <View style={styles.redView}/>
         <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
       </View>
     );
   }
@@ -16,18 +48,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    flexDirection: 'row'
+    flexDirection: 'row',
+    justifyContent: 'flex-start', //flex-start, flex-end, space-between, space-around, center
+    alignItems: 'center',
+    flexWrap: 'nowrap'
   },
   redView: {
     // flex: 1,
     width: 50,
     height: 50,
-    backgroundColor: 'red'
+    backgroundColor: 'red',
+    // alignSelf: 'flex-start'
   },
   yellowView: {
     // flex: 1,
     width: 50,
     height: 50,
-    backgroundColor: 'yellow'
+    backgroundColor: 'yellow',
+    // alignSelf: 'flex-end'
   }
 });

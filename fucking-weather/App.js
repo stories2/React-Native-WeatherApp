dff4b1438572f46502cc4bea70274663d5005b41
Kim HyunWoo <stories2@naver.com>
2019-04-30 14:13:10 +0900

    로딩 컴포넌트 추가

--- fucking-weather/App.js
+++ fucking-weather/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <ActivityIndicator />
         <Text>Hello World!</Text>
       </View>
     );
